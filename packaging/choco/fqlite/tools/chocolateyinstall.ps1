3765832b9c4e6efafd4effb49f516c47a89d8ed5
Jan Schneider <wladi.schneider13@googlemail.com>
2024-08-01 15:05:36 +0200

    Clean up choco install files

--- packaging/choco/fqlite/tools/chocolateyinstall.ps1
+++ packaging/choco/fqlite/tools/chocolateyinstall.ps1
@@ -1,59 +1,25 @@
-﻿# IMPORTANT: Before releasing this package, copy/paste the next 2 lines into PowerShell to remove all comments from this file:
-#   $f='c:\path\to\thisFile.ps1'
-#   gc $f | ? {$_ -notmatch "^\s*#"} | % {$_ -replace '(^.*?)\s*?[^``]#.*','$1'} | Out-File $f+".~" -en utf8; mv -fo $f+".~" $f
-
-# 1. See the _TODO.md that is generated top level and read through that
-# 2. Follow the documentation below to learn how to create a package for the package type you are creating.
-# 3. In Chocolatey scripts, ALWAYS use absolute paths - $toolsDir gets you to the package's tools directory.
-$ErrorActionPreference = 'Stop' # stop on all errors
+﻿
+$ErrorActionPreference = 'Stop'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-# Internal packages (organizations) or software that has redistribution rights (community repo)
-# - Use `Install-ChocolateyInstallPackage` instead of `Install-ChocolateyPackage`
-#   and put the binaries directly into the tools folder (we call it embedding)
-#$fileLocation = Join-Path $toolsDir 'NAME_OF_EMBEDDED_INSTALLER_FILE'
-# If embedding binaries increase total nupkg size to over 1GB, use share location or download from urls
-#$fileLocation = '\\SHARE_LOCATION\to\INSTALLER_FILE'
-# Community Repo: Use official urls for non-redist binaries or redist where total package size is over 200MB
-# Internal/Organization: Download from internal location (internet sources are unreliable)
-$url        = 'https://github.com/bocian67/fqlite/releases/download/2.6.5/fqlite-windows-latest' # download url, HTTPS preferred
-$url64      = 'https://github.com/bocian67/fqlite/releases/download/2.6.5/fqlite-windows-latest' # 64bit URL here (HTTPS preferred) or remove - if installer contains both (very rare), use $url
+$url        = 'https://github.com/bocian67/fqlite/releases/download/2.6.5/fqlite-windows-latest'
+$url64      = 'https://github.com/bocian67/fqlite/releases/download/2.6.5/fqlite-windows-latest'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
-  fileType      = 'exe' #only one of these: exe, msi, msu
+  fileType      = 'exe'
   url           = $url
   url64bit      = $url64
-  #file         = $fileLocation
 
-  softwareName  = 'fqlite*' #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
+  softwareName  = 'fqlite*'
 
-  # Checksums are required for packages which will be hosted on the Chocolatey Community Repository.
-  # To determine checksums, you can get that from the original site if provided.
-  # You can also use checksum.exe (choco install checksum) and use it
-  # e.g. checksum -t sha256 -f path\to\file
   checksum      = ''
-  checksumType  = 'sha256' #default is md5, can also be sha1, sha256 or sha512
+  checksumType  = 'sha256'
   checksum64    = ''
-  checksumType64= 'sha256' #default is checksumType
+  checksumType64= 'sha256'
 
-  # MSI
-  # silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`"" # ALLUSERS=1 DISABLEDESKTOPSHORTCUT=1 ADDDESKTOPICON=0 ADDSTARTMENU=0
   validExitCodes= @(0, 3010, 1641)
-  # OTHERS
-  # Uncomment matching EXE type (sorted by most to least common)
-  silentArgs   =  '/quiet' #'/q'           # WiX
-  #silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-' # Inno Setup
-  #silentArgs   = '/s'           # InstallShield
-  #silentArgs   = '/s /v"/qn"'   # InstallShield with MSI
-  #silentArgs   = '/s'           # Wise InstallMaster
-  #silentArgs   = '-s'           # Squirrel
-  #silentArgs   = '-q'           # Install4j
-  #silentArgs   = '-s'           # Ghost
-  # Note that some installers, in addition to the silentArgs above, may also need assistance of AHK to achieve silence.
-  #silentArgs   = ''             # none; make silent with input macro script like AutoHotKey (AHK)
-                                 #       https://community.chocolatey.org/packages/autohotkey.portable
-  #validExitCodes= @(0) #please insert other valid exit codes here
+  silentArgs   =  '/quiet'
 }
 
 Install-ChocolateyPackage @packageArgs
