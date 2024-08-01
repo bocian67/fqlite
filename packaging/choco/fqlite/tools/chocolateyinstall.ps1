16d35524ac99054f2f5b039478d34ca203c59df0
Jan Schneider <wladi.schneider13@googlemail.com>
2024-08-01 18:52:10 +0200

    Create choco package as action

--- packaging/choco/fqlite/tools/chocolateyinstall.ps1
+++ packaging/choco/fqlite/tools/chocolateyinstall.ps1
@@ -1,21 +1,17 @@
 ﻿
 $ErrorActionPreference = 'Stop'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://github.com/bocian67/fqlite/releases/download/2.6.5/fqlite-windows-latest'
-$url64      = 'https://github.com/bocian67/fqlite/releases/download/2.6.5/fqlite-windows-latest'
+$url64      = "https://github.com/bocian67/fqlite/releases/download/$($env:VERSION)/fqlite-windows-latest"
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
   fileType      = 'exe'
-  url           = $url
   url64bit      = $url64
 
   softwareName  = 'fqlite*'
 
-  checksum      = ''
-  checksumType  = 'sha256'
-  checksum64    = ''
+  checksum64    = $env:FILE_HASH
   checksumType64= 'sha256'
 
   validExitCodes= @(0, 3010, 1641)
